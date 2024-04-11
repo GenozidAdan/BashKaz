2be6c881bd894a0fe6fa9125d78f2911b7425fb7
DramaDGeno <acostakazumi@gmail.com>
2024-04-10 19:50:12 -0600

    Añadi lineas de protected

--- Models/MascotaModel.php
+++ Models/MascotaModel.php
@@ -44,5 +44,32 @@ class MascotaModel extends Model {
 		return $this->update($id, $data);
 	}
 	//Fin de añadimiento
+
+		// Define la relación con el modelo DuenoModel
+		protected $with = ['dueno'];
+
+		// Define la relación inversa (opcional)
+		public function dueno() {
+			return $this->belongsTo(DuenoModel::class, 'id_dueno', 'id_dueno');
+		}   
+	
+		//Añadir aquí las funciones personalizadas para MascotaModel
+		public function getMascotas() {
+			return $this->findAll();
+		}
+	
+		public function getMascota($id) {
+			return $this->find($id);
+		}
+	
+		public function insertarMascota($data) {
+			return $this->insert($data);
+		}
+	
+		public function actualizarMascota($id, $data) {
+			return $this->update($id, $data);
+		}
+		//Fin de añadimiento
+		
 	
 }
